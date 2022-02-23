4dfd1775b02118d2036a3210a980b0d47d140e38
Reinis Lubins <reinis.lubins@gmail.com>
2022-02-23 13:55:43 +0200

    Finished: Ex#1, Ex#2, Ex#3

--- src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
+++ src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
@@ -1,26 +1,36 @@
 package io.codelex.typesandvariables.practice;
 
-public class MoreVariablesAndPrinting {
-    public static void main(String[] args) {
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
+public class MoreVariablesAndPrinting
+{
+    public static void main(String[] args)
+    {
+        String Name;
+        String Eyes;
+        String Teeth;
+        String Hair;
+        int Age;
+        int Height;
+        int Weight;
 
-        myName = "Zed A. Shaw";
-        myAge = 35;
-        myHeight = 74;  // inches
-        myWeight = 180; // lbs
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        Name = "Zed A. Shaw";
+        Age = 35;
+        Height = 74;  // inches
+        Weight = 180; // lbs
+        Eyes = "Blue";
+        Teeth = "White";
+        Hair = "Brown";
+        double HeightToCm = Height * 2.54;
+        double WeightToKg = Weight * 0.453592;
+        HeightToCm = Math.round(HeightToCm * 100.0) / 100.0;
+        WeightToKg = Math.round(WeightToKg * 100.0) / 100.0;
 
-        System.out.println("Let's talk about " + myName + ".");
-        System.out.println("He's " + myHeight + " inches tall.");
-        System.out.println("He's " + myWeight + " pounds heavy.");
+        System.out.println("Let's talk about " + Name + ".");
+        System.out.println("He's " + HeightToCm + " inches tall.");
+        System.out.println("He's " + WeightToKg + " pounds heavy.");
         System.out.println("Actually, that's not too heavy.");
-        System.out.println("He's got " + myEyes + " eyes and " + myHair + " hair.");
-        System.out.println("His teeth are usually " + myTeeth + " depending on the coffee.");
+        System.out.println("He's got " + Eyes + " eyes and " + Hair + " hair.");
+        System.out.println("His teeth are usually " + Teeth + " depending on the coffee.");
 
-        System.out.println("If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                + " I get " + (myAge + myHeight + myWeight) + ".");
+        System.out.println("If I add " + Age + ", " + HeightToCm + ", and " + WeightToKg + " I get " + (Age + HeightToCm + WeightToKg) + ".");
     }
 }
--- src/main/java/io/codelex/typesandvariables/practice/VariablesAndNames.java
+++ src/main/java/io/codelex/typesandvariables/practice/VariablesAndNames.java
@@ -1,5 +1,7 @@
 package io.codelex.typesandvariables.practice;
 
+import java.text.DecimalFormat;
+
 public class VariablesAndNames {
     public static void main(String[] args) {
         int cars;
@@ -19,12 +21,13 @@ public class VariablesAndNames {
         carsDriven = drivers;
         carpoolCapacity = carsDriven * seatsInACar;
         averagePassengersPerCar = passengers / carsDriven;
+        DecimalFormat df = new DecimalFormat("0.00");
 
         System.out.println("There are " + cars + " cars available.");
         System.out.println("There are only " + drivers + " drivers available.");
         System.out.println("There will be " + carsNotDriven + " empty cars today.");
         System.out.println("We can transport " + carpoolCapacity + " people today.");
         System.out.println("We have " + passengers + " to carpool today.");
-        System.out.println("We need to put about " + averagePassengersPerCar + " in each car.");
+        System.out.println("We need to put about " + df.format(averagePassengersPerCar) + " in each car.");
     }
 }
