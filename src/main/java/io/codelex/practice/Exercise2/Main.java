a1a80d553c156a2c76c03e962da116624bae1a19
Reinis Lubins <reinis.lubins@gmail.com>
2022-03-11 11:30:44 +0200

    1st Commit - Date/time exercises

---
+++ src/main/java/io/codelex/practice/Exercise2/Main.java
@@ -0,0 +1,7 @@
+package io.codelex.practice.Exercise2;
+
+public class Main {
+    public static void main(String[] args) {
+        Update.runProgram();
+    }
+}
---
+++ src/main/java/io/codelex/practice/Exercise3/Main.java
@@ -0,0 +1,7 @@
+package io.codelex.practice.Exercise3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
