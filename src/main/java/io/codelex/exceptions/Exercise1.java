c13aeb967824051a097709c41e9c39a4212ca1c9
Reinis Lubins <reinis.lubins@gmail.com>
2022-03-22 15:12:24 +0200

    2nd Commit - Exceptions

--- src/main/java/io/codelex/exceptions/Exercise1.java
+++ src/main/java/io/codelex/exceptions/Exercise1.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class Exercise1 {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        int num = 0;
 
         while (true) {
-            System.out.println("Enter the numerator");
-            int numerator = scanner.nextInt();
-            System.out.println("Enter the divisor");
-            int divisor = scanner.nextInt();
-            System.out.println(numerator + " / " + divisor + " = " + numerator / divisor);
+            try {
+                System.out.println("Enter the numerator");
+                String numeratorString = scanner.next();
+                if (numeratorString.charAt(0) == 'q' || numeratorString.charAt(0) == 'Q') {
+                    break;
+                } else {
+                    int numerator = Integer.parseInt(numeratorString);
+                    num = numerator;
+                    System.out.println("Enter the divisor");
+                    int divisor = scanner.nextInt();
+                    System.out.println(numerator + " / " + divisor + " = " + numerator / divisor);
+                }
+            } catch (ArithmeticException e) {
+                System.out.printf("You can't divide %d by 0\n", num);
+            } catch (Exception e) {
+                System.out.println("You entered bad data.\n" + "Please try again.");
+                scanner.nextLine();
+            }
         }
     }
 }
--- src/main/java/io/codelex/exceptions/Exercise2.java
+++ src/main/java/io/codelex/exceptions/Exercise2.java
@@ -2,11 +2,32 @@ package io.codelex.exceptions;
 
 public class Exercise2 {
     public void methodA() throws ArithmeticException {
+        //System.out.println(10 / 0);
+        try {
+            methodB();
+        } catch (ArithmeticException e) {
+            System.out.println("Exception at method A");
+        }
     }
 
     public void methodB() throws ArithmeticException {
+        try {
+            methodC();
+        } catch (ArithmeticException e) {
+            System.out.println("Exception at method B");
+        }
     }
 
     public void methodC() throws ArithmeticException {
+        try {
+            System.out.println(20 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception at method C: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Exercise2 test = new Exercise2();
+        test.methodA();
     }
 }
