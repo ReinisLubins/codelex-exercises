15734052008438cc4f602e4893be8363114b31be
Reinis Lubins <reinis.lubins@gmail.com>
2022-03-22 17:39:56 +0200

    1st Commit - Collections

--- src/main/java/io/codelex/collections/practice/Exercise1.java
+++ src/main/java/io/codelex/collections/practice/Exercise1.java
@@ -1,5 +1,7 @@
 package io.codelex.collections.practice;
 
+import java.util.*;
+
 /**
  * Origination:
  * Audi -> Germany
@@ -14,9 +16,18 @@ public class Exercise1 {
         String[] array = {"Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla"};
 
         //todo - replace array with an ArrayList and print out the results
+        List<String> newList = Arrays.asList(array);
+        System.out.println(newList);
 
         //todo - replace array with a HashSet and print out the results
+        Set<String> hashList = new HashSet<>(Arrays.asList(array));
+        System.out.println(hashList);
 
         //todo - replace array with a HashMap (use brand as key and origination as value) an print out the results
+        Map<Integer, String> hashMap = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            hashMap.put(i, array[i]);
+        }
+        System.out.println(hashMap);
     }
 }
